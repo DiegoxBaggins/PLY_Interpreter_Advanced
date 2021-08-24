a59f6efbd62155ea2622969f266ad831df0a8fd9
DiegoxBaggins <deigosierra@gmail.com>
2021-08-24 01:27:46 -0600

    maquetacion de las 3 paginas completa, aun sin funcionamiento logico

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def slash():  # put application's code here
+    return redirect(url_for('home'))
+
+
+@app.route('/home')
+def home():  # put application's code here
     return render_template('index.html')
+
+
+@app.route('/analisis')
+def analisis():  # put application's code here
+    return render_template('analisis.html')
+
+
+@app.route('/reports')
+def reports():  # put application's code here
+    return render_template('reports.html')
 
 
 if __name__ == '__main__':
